6fa6796919f4c89667884f367325a436a54ffc9d
amruthabandaru <amruthabandaru341@gmail.com>
2025-10-22 10:16:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18 MB (100.00%)

---
+++ 0300-longest-increasing-subsequence/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLIS(self, nums):
+        dp = []
+        import bisect
+        for n in nums:
+            i = bisect.bisect_left(dp, n)
+            if i == len(dp):
+                dp.append(n)
+            else:
+                dp[i] = n
+        return len(dp)
+
