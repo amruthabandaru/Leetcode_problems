5ff56d575ec47cac38f2aec71e7f6b6246014336
amruthabandaru <amruthabandaru341@gmail.com>
2026-06-12 05:55:35 +0000

    Sync LeetCode submission Runtime - 47 ms (89.35%), Memory - 22.5 MB (86.24%)

--- 0141-linked-list-cycle/solution.py
+++ 0141-linked-list-cycle/solution.py
@@ -1,10 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution:
-    def hasCycle(self, head):
-        slow = fast = head
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow=head
+        fast=head
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
                 return True
         return False
-
